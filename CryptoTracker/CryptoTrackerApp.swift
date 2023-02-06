2a9c1cde4f25bd8bc94af05d7dc9ac812c33e420
anna1901 <anna.olak1901@gmail.com>
2023-02-06 17:19:44 +0100

    Initial commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Anna Olak on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
